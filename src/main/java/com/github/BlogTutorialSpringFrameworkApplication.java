1562221496f5a2b1a109fbbe6e8839f5d801365d
Leonardus Dani Novianto <leonardus.dani@gmail.com>
2016-12-16 15:08:40 +0700

    Add Spring Boot for template project

---
+++ src/main/java/com/github/BlogTutorialSpringFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTutorialSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTutorialSpringFrameworkApplication.class, args);
+	}
+}
